ac3ecb747fc2b64c7e92b8039849ec396ddf5b67
Nocturnal <mister.cheban@gmail.com>
2016-06-18 14:29:31 +0400

    JC-2291 List User Groups
    Added checking permission in
    GroupController.java using PermissionService.java
    and intercept-url for group-list url path.

--- jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/GroupController.java
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/GroupController.java
@@ -1,30 +1,48 @@
 package org.jtalks.jcommune.web.controller;
 
+import org.jtalks.common.model.entity.Group;
+import org.jtalks.common.model.permissions.GeneralPermission;
 import org.jtalks.jcommune.service.GroupService;
+import org.jtalks.jcommune.service.security.AclClassName;
+import org.jtalks.jcommune.service.security.PermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Map;
+
 /**
  * This controller handles actions with groups
  * like display list of groups and number of users in it.
  *
  * @author Oleg Tkachenko
+ * @author Evgeniy Cheban
  */
 
 @Controller
 public class GroupController {
     private final GroupService groupService;
 
+    private final PermissionService permissionService;
+
     @Autowired
-    public GroupController(GroupService groupService) {
+    public GroupController(GroupService groupService, PermissionService permissionService) {
         this.groupService = groupService;
+        this.permissionService = permissionService;
     }
 
+    /**
+     * Find groups with number of users inside them
+     * with checking permission using {@link org.jtalks.jcommune.service.security.PermissionService}
+     */
     @RequestMapping(value = "/group/list", method = RequestMethod.GET)
     public ModelAndView showGroupsWithNumberOfUsers() {
-        return new ModelAndView("userGroups").addObject("groups", groupService.getAllGroupsWithNumOfUsers());
+        Map<Group, Long> withNumOfUsers = groupService.getAllGroupsWithNumOfUsers();
+        for (Group group : withNumOfUsers.keySet()) {
+            permissionService.checkPermission(group.getId(), AclClassName.GROUP, GeneralPermission.ADMIN);
+        }
+        return new ModelAndView("userGroups").addObject("groups", withNumOfUsers);
     }
 }
